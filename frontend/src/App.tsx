d1741290e955aa9056269812a448896548508420
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-01-15 16:33:13 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./componentes/Navbar/navbar";
 
 
-function App() {
+export default function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
-export default App;
+ 
---
+++ frontend/src/componentes/Navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from "../../recursos/img/github.svg"
+import './styles.css'
+
+export default function Navbar(){
+    return(
+    <header>
+        <nav className="container">
+            <div className="loc-nav-content">
+                <h1>Loc Casas</h1>
+                <a href="https://github.com/vanessa-maganhoto" target="_blank" rel="noreferrer">
+                    <div className="loc-contact-container">
+                        <GithubIcon/>
+                        <p className="loc-contact-link">/vanessa-maganhoto</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+    )
+}
